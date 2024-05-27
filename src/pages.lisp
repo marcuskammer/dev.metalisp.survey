be973ad864de13c304e475d33351993fb9b4ba71
Marcus Kammer <marcus.kammer@mailbox.org>
2024-05-27 21:35:29 +0200

    Add german language sus form

--- src/pages.lisp
+++ src/pages.lisp
@@ -119,3 +119,106 @@
 
            (btn-primary (:type "submit")
              (find-l10n "submit" *html-lang* *l10n*)))))
+
+
+(defun sus-form-de ()
+  (with-page (:title "SUS Formular" :main-con t)
+    (navbar-de)
+    (:h2 "Usability Feedback Formular")
+    (:p "Bitte füllen Sie die folgende Formular aus und klicken Sie auf die Schaltfläche 'Senden'.")
+    (:form :action "/submit"
+           :method "post"
+           :class (dev.metalisp.sbt/utility:spacing :property "m" :side "y" :size 5)
+           (multi-form
+             (:ask "Ich denke, dass ich dieses System häufig nutzen möchte."
+              :group "sus-1"
+              :style "list-style:none;"
+              :choices (:single "1 Strongly Disagree"
+                                "2 Disagree"
+                                "3 Neither Agree nor Disagree"
+                                "4 Agree"
+                                "5 Strongly Agree"))
+
+             (:ask "Ich fand das System unnötig komplex."
+              :group "sus-2"
+              :style "list-style:none;"
+              :choices (:single "1 Strongly Disagree"
+                                "2 Disagree"
+                                "3 Neither Agree nor Disagree"
+                                "4 Agree"
+                                "5 Strongly Agree"))
+
+             (:ask "Ich fand das System einfach zu bedienen."
+              :group "sus-3"
+              :style "list-style:none;"
+              :choices (:single "1 Strongly Disagree"
+                                "2 Disagree"
+                                "3 Neither Agree nor Disagree"
+                                "4 Agree"
+                                "5 Strongly Agree"))
+
+             (:ask "Ich glaube, ich bräuchte die Unterstützung einer technischen Person, um dieses System nutzen zu können."
+              :group "sus-4"
+              :style "list-style:none;"
+              :choices (:single "1 Strongly Disagree"
+                                "2 Disagree"
+                                "3 Neither Agree nor Disagree"
+                                "4 Agree"
+                                "5 Strongly Agree"))
+
+             (:ask "Ich fand, dass die verschiedenen Funktionen in diesem System gut integriert waren."
+              :group "sus-5"
+              :style "list-style:none;"
+              :choices (:single "1 Strongly Disagree"
+                                "2 Disagree"
+                                "3 Neither Agree nor Disagree"
+                                "4 Agree"
+                                "5 Strongly Agree"))
+
+             (:ask "Ich fand, dass dieses System zu widersprüchlich war."
+              :group "sus-6"
+              :style "list-style:none;"
+              :choices (:single "1 Strongly Disagree"
+                                "2 Disagree"
+                                "3 Neither Agree nor Disagree"
+                                "4 Agree"
+                                "5 Strongly Agree"))
+
+             (:ask "Ich könnte mir vorstellen, dass die meisten Menschen sehr schnell lernen würden, mit diesem System umzugehen."
+              :group "sus-7"
+              :style "list-style:none;"
+              :choices (:single "1 Strongly Disagree"
+                                "2 Disagree"
+                                "3 Neither Agree nor Disagree"
+                                "4 Agree"
+                                "5 Strongly Agree"))
+
+             (:ask "Ich fand das System sehr umständlich zu bedienen."
+              :group "sus-8"
+              :style "list-style:none;"
+              :choices (:single "1 Strongly Disagree"
+                                "2 Disagree"
+                                "3 Neither Agree nor Disagree"
+                                "4 Agree"
+                                "5 Strongly Agree"))
+
+             (:ask "Ich fühlte mich sehr sicher im Umgang mit dem System."
+              :group "sus-9"
+              :style "list-style:none;"
+              :choices (:single "1 Strongly Disagree"
+                                "2 Disagree"
+                                "3 Neither Agree nor Disagree"
+                                "4 Agree"
+                                "5 Strongly Agree"))
+
+             (:ask "Ich musste eine Menge Dinge lernen, bevor ich mit diesem System loslegen konnte."
+              :group "sus-10"
+              :style "list-style:none;"
+              :choices (:single "1 Strongly Disagree"
+                                "2 Disagree"
+                                "3 Neither Agree nor Disagree"
+                                "4 Agree"
+                                "5 Strongly Agree")))
+
+           (btn-primary (:type "submit")
+             (find-l10n "submit" *html-lang* *l10n*)))))
