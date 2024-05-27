1a071bbd7bcc24301d6c3083bf631591f51ffa23
Marcus Kammer <marcus.kammer@mailbox.org>
2024-05-27 16:15:50 +0200

    Move gui macros to partials module

--- src/pages.lisp
+++ src/pages.lisp
@@ -1,52 +1,22 @@
 (in-package :dev.metalisp.survey)
 
-(defmacro navbar-brand (src width)
-  `(spinneret:with-html
-     (:a :class "navbar-brand"
-         :href "#"
-         (:img :src ,src
-               :alt "Company Logo"
-               :width ,width))))
-
-(defmacro navbar-toggle (target)
-  `(spinneret:with-html
-     (:button :class "navbar-toggler"
-              :type "button"
-              :data-bs-toggle "collapse"
-              :data-bs-target ,target
-              :aria-controls ,target
-              :aria-expanded "false"
-              :aria-label "Toggle Navigation"
-              (:span :class "navbar-toggler-icon"))))
-
-(defmacro navbar-nav (id &body body)
-  `(spinneret:with-html
-     (:div :class "collapse navbar-collapse"
-           :id ,id
-           (:ul :class "navbar-nav"
-                ,@(loop for navitem on body by #'cddr
-                        collect
-                        `(:li :class "nav-item"
-                              (:a :class "nav-link"
-                                  :href ,(first navitem)
-                                  ,(second navitem))))))))
-
-(defmacro navbar (&body body)
-  `(spinneret:with-html
-     (:nav :class "navbar bg-body-tertiary navbar-expand-sm mb-5"
-           (:div :class "container-fluid"
-                 ,@body))))
-
-(defmacro navbar* (id brand-src brand-width &body body)
-  `(navbar (navbar-brand ,brand-src ,brand-width)
-     (navbar-toggle ,id)
-     (navbar-nav ,id ,@body)))
+(defmacro navbar-de ()
+  (let ((id "#mainNav"))
+    `(navbar (navbar-brand "/src/assets/company_logo.png" 30)
+       (navbar-toggle ,id)
+       (navbar-nav ,id "/" "Home" "/imprint" "Impressum"))))
 
 (defmacro navbar-en ()
   (let ((id "#mainNav"))
-    `(navbar (navbar-brand "/src/assets/company_logo.png" 100)
+    `(navbar (navbar-brand "/src/assets/company_logo.png" 30)
        (navbar-toggle ,id)
        (navbar-nav ,id "/" "Home" "/imprint" "Imprint"))))
+
+(defun home ()
+  (with-page (:title "Survey" :main-con t)
+    (navbar-de)
+    (:h2 "Surveys")
+    (:ul (:li (:a :href "/sus?lang=en" "SUS en")))))
 
 (defun sus-form-en ()
   (with-page (:title "SUS Form" :main-con t)
---
+++ src/partials/_navbar.lisp
@@ -0,0 +1,43 @@
+(in-package :dev.metalisp.survey)
+
+(defmacro navbar-brand (src width)
+  `(spinneret:with-html
+     (:a :class "navbar-brand"
+         :href "#"
+         (:img :src ,src
+               :alt "Company Logo"
+               :width ,width))))
+
+(defmacro navbar-toggle (target)
+  `(spinneret:with-html
+     (:button :class "navbar-toggler"
+              :type "button"
+              :data-bs-toggle "collapse"
+              :data-bs-target ,target
+              :aria-controls ,target
+              :aria-expanded "false"
+              :aria-label "Toggle Navigation"
+              (:span :class "navbar-toggler-icon"))))
+
+(defmacro navbar-nav (id &body body)
+  `(spinneret:with-html
+     (:div :class "collapse navbar-collapse"
+           :id ,id
+           (:ul :class "navbar-nav"
+                ,@(loop for navitem on body by #'cddr
+                        collect
+                        `(:li :class "nav-item"
+                              (:a :class "nav-link"
+                                  :href ,(first navitem)
+                                  ,(second navitem))))))))
+
+(defmacro navbar (&body body)
+  `(spinneret:with-html
+     (:nav :class "navbar bg-body-tertiary navbar-expand-sm mb-5"
+           (:div :class "container-fluid"
+                 ,@body))))
+
+(defmacro navbar* (id brand-src brand-width &body body)
+  `(navbar (navbar-brand ,brand-src ,brand-width)
+     (navbar-toggle ,id)
+     (navbar-nav ,id ,@body)))
