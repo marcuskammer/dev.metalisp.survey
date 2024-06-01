80065852ea0bd7a64ccea4b448eae390e9b849ea
Marcus Kammer <marcus.kammer@mailbox.org>
2024-06-01 16:26:59 +0200

    Remove comments

--- src/handlers.lisp
+++ src/handlers.lisp
@@ -1,31 +1,5 @@
 (in-package :ml-survey)
 
-;; (define-easy-handler (sus :uri "/sus") (lang)
-;;   (setf *html-lang* lang)
-;;   (return-sus-form lang))
-
-;; (define-easy-handler (submit :uri "/submit") nil
-;;   (setf (content-type*) "text/plain")
-;;   (let* ((post-params (post-parameters* *request*))
-;;          (stored-response (load-response (make-db-path (today) "_submit-db.lisp")))
-;;          (response (reverse (push (list (now) post-params) stored-response))))
-;;     (store-response (make-db-path (today) "_submit-db.lisp") response)
-;;     (format nil "~A" response)))
-
-;; (defun starts-with-subseq (subseq seq)
-;;   "Check if the sequence SEQ starts with the subsequence SUBSEQ."
-;;   (let ((subseq-length (length subseq)))
-;;     (and (<= subseq-length (length seq))
-;;          (string= subseq (subseq seq 0 subseq-length)))))
-
-;; (defun survey-uri-p (request)
-;;   "Predicate function to check if the request URI matches the survey pattern.
-;; The URI should start with \"/survey/\" followed by a numeric ID."
-;;   (let* ((uri (hunchentoot:request-uri request))
-;;          (id (subseq uri (length "/survey/"))))
-;;     (and (starts-with-subseq "/survey/" uri)
-;;          (every #'digit-char-p id))))
-
 (defun split-uri (uri)
   (remove-if #'string-empty-p (uiop:split-string uri :separator "/")))
 
