00df834718a295a5f9eb998a0c91869f0cff06d2
Marcus Kammer <marcus.kammer@mailbox.org>
2024-06-02 22:52:47 +0200

    Add whitespace

--- src/handlers/survey.lisp
+++ src/handlers/survey.lisp
@@ -4,13 +4,16 @@
   (labels ((survey-fn (action)
              (case action
                (id (second (split-uri uri)))
+
                (id-p (let ((ids (mapcar #'car (load-response (make-surveys-db-path)))))
                        (if (member (parse-integer (survey-fn 'id)) ids) t nil)))
+
                (uri-p (let ((parts (split-uri uri)))
                         (and (= (length parts) 2)
                              (string= (first parts) "survey")
                              (every #'digit-char-p (second parts))
                              (survey-fn 'id-p))))
+
                (properties (first (rest (assoc (parse-integer (survey-fn 'id))
                                                (load-response (make-surveys-db-path)))))))))
     #'survey-fn))
