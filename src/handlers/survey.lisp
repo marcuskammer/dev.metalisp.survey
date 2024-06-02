615c547b19a32698f0f1b89f573939cf070e079f
Marcus Kammer <marcus.kammer@mailbox.org>
2024-06-02 17:04:48 +0200

    Define a make-survey function

--- src/handlers/survey.lisp
+++ src/handlers/survey.lisp
@@ -29,6 +29,21 @@
   (check-type id integer)
   (first (rest (assoc id (load-response (make-surveys-db-path))))))
 
+(defun make-survey (uri)
+  (labels ((survey-fn (action)
+             (case action
+               (id (second (split-uri uri)))
+               (id-p (let ((ids (mapcar #'car (load-response (make-surveys-db-path)))))
+                       (if (member (survey-fn 'id) ids) t nil)))
+               (uri-p (let ((parts (split-uri uri)))
+                        (and (= (length parts) 2)
+                             (string= (first parts) "survey")
+                             (every #'digit-char-p (second parts))
+                             (survey-fn 'id-p))))
+               (properties (let ((survey-id (parse-integer (survey-fn 'id))))
+                             (first (rest (assoc survey-id (load-response (make-surveys-db-path))))))))))
+    #'survey-fn))
+
 (define-easy-handler (survey :uri #'survey-uri) ()
-  (let ((id (survey-id (request-uri*))))
-    (ml-survey/views:survey id (survey-properties (parse-integer id)))))
+  (let ((survey (make-survey (request-uri*))))
+    (ml-survey/views:survey (funcall survey 'id) (funcall survey 'properties))))
