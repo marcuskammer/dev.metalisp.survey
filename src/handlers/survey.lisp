8ec67cd985f25b39536e3e6212cf46819882aa0d
Marcus Kammer <marcus.kammer@mailbox.org>
2024-06-02 21:10:08 +0200

    Fix survey function call

--- src/handlers/survey.lisp
+++ src/handlers/survey.lisp
@@ -3,11 +3,12 @@
 (defun survey-uri-p (uri)
   "Check if the request URI matches the pattern '/survey/<numeric>'"
   (check-type uri string)
-  (let ((parts (split-uri uri)))
+  (let ((parts (split-uri uri))
+        (survey (make-survey uri)))
     (and (= (length parts) 2)
          (string= (first parts) "survey")
          (every #'digit-char-p (second parts))
-         (survey-id-p (parse-integer (second parts))))))
+         (funcall survey 'id-p))))
 
 (defun survey-uri (request)
   (let ((uri (request-uri request)))
