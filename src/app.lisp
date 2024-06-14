b13b79731570f3d31193a204dba3cb3cd55a474c
Marcus Kammer <marcus.kammer@mailbox.org>
2024-06-14 17:46:12 +0200

    Set access log

--- src/app.lisp
+++ src/app.lisp
@@ -3,11 +3,12 @@
 (defun set-default-directory (directory)
   (setf *default-pathname-defaults* (truename (merge-pathnames directory))))
 
-(defun create-server (name port &key address document-root)
+(defun create-server (name port &key address document-root access-log-destination)
   (let ((acceptor (make-instance 'hunchentoot:easy-acceptor
                                  :address address
                                  :name name
                                  :document-root document-root
+                                 :access-log-destination access-log-destination
                                  :port port)))
     acceptor))
 
@@ -26,7 +27,9 @@
 (defvar *app* (create-server 'app
                              8080
                              :document-root
-                             "public/"))
+                             "public/"
+                             :access-log-destination
+                             (uiop:merge-pathnames* #P"access.log" (app-dir))))
 
 (defun start ()
   (start-server *app*))
