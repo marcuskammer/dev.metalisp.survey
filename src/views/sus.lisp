8613aac968cd3e9b514b495fa46a28dc1fd1e9c8
Marcus Kammer <marcus.kammer@mailbox.org>
2024-06-15 21:18:25 +0200

    Add type checking

--- src/views/sus.lisp
+++ src/views/sus.lisp
@@ -2,6 +2,8 @@
 
 (defun load-form (lang form-file-name)
   "Load a Lisp file containing form definitions."
+  (check-type lang string)
+  (check-type form-file-name string)
   (let* ((relative-path (concatenate 'string "src/views/forms/" lang "/"))
          (full-path (uiop:merge-pathnames* relative-path (uiop:getcwd)))
          (form-path (uiop:merge-pathnames* form-file-name full-path)))
