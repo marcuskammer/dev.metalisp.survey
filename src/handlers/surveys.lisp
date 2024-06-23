ae31001fcfdde983017fa8796a5143ab1dd4f41c
Marcus Kammer <marcus.kammer@mailbox.org>
2024-06-23 15:16:25 +0200

    Optimize code readability

--- src/handlers/surveys.lisp
+++ src/handlers/surveys.lisp
@@ -1,9 +1,9 @@
 (in-package :ml-survey/handlers)
 
 (define-easy-handler (surveys :uri "/") nil
-  (ml-survey/views:surveys (mapcar (lambda (x)
-                                     (make-instance 'ml-survey:survey
-                                                    :id (format nil
-                                                                "~a"
-                                                                (first x))))
-                                   (load-response (make-surveys-db-file)))))
+  (let* ((survey-db (load-response (make-surveys-db-file)))
+         (list-of-surveys (mapcar (lambda (x)
+                                    (make-instance 'ml-survey:survey
+                                                   :id (format nil "~a" (first x))))
+                                  survey-db)))
+    (ml-survey/views:surveys list-of-surveys)))
