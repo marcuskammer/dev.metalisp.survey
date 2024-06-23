b70b23bfcc7fd471ced3e4de2839efc55f3d3a95
Marcus Kammer <marcus.kammer@mailbox.org>
2024-06-23 13:07:45 +0200

    Add docstring to macro

--- src/views/sus.lisp
+++ src/views/sus.lisp
@@ -13,10 +13,17 @@
   nil)
 
 (defmacro with-form (&body body)
+  "Create a standardized HTML form wrapped in a <main> tag with a pre-defined
+class and structure, using the Spinneret library. The form is designed to be
+used within a web application served by Hunchentoot, utilizing common layout
+and localization practices. The macro automatically sets the form’s action to
+the current request URI and expects certain functions and variables to be
+available in its environment for full functionality."
   `(spinneret:with-html
     (:main :id "main-content"
            :class "container my-5"
            (:p "Please fill out the following forms and press the submit button.")
+           ;; action is defined as hunchentoot:request-uri* function
            (:form :action (request-uri*)
                   :method "post"
                   :class (spacing :property "m" :side "y" :size 5)
