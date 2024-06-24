ab79c46a9a63d5b0e7d291c964ce54e3176d184e
Marcus Kammer <marcus.kammer@mailbox.org>
2024-06-24 17:24:48 +0200

    Start defining post and get functions

--- src/handlers/new-survey.lisp
+++ src/handlers/new-survey.lisp
@@ -1,4 +1,12 @@
 (in-package :ml-survey/handlers)
 
+(defun process-new-survey-get ())
+
+(defun process-new-survey-post (request)
+  (let ((post-params (post-parameters* *request*))
+        (uid (generate-uuid))
+        (stored-surveys (load-response (make-surveys-db-file))))
+    (store-response (make-surveys-db-file) (push (list uid post-params) stored-surveys))))
+
 (define-easy-handler (new-survey :uri "/new-survey") nil
   (ml-survey/views:new-survey))
