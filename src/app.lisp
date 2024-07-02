0bc238b977d76d742419d7baf7aa24b99f9e002d
Marcus Kammer <marcus.kammer@mailbox.org>
2024-07-02 21:14:41 +0200

    Fix code indent

--- src/app.lisp
+++ src/app.lisp
@@ -42,15 +42,15 @@
   ;; warning: hardcoded "hunchentoot".
   ;; You can simply run (sleep most-positive-fixnum)
   (handler-case (bt:join-thread (find-if (lambda (th)
-                                            (search "hunchentoot" (bt:thread-name th)))
+                                           (search "hunchentoot" (bt:thread-name th)))
                                          (bt:all-threads)))
     ;; Catch a user's C-c
     (#+sbcl sb-sys:interactive-interrupt
-      #+ccl  ccl:interrupt-signal-condition
-      #+clisp system::simple-interrupt-condition
-      #+ecl ext:interactive-interrupt
-      #+allegro excl:interrupt-signal
-      () (progn
-           (format *error-output* "Aborting.~&")
-           (uiop:quit)))
+     #+ccl  ccl:interrupt-signal-condition
+     #+clisp system::simple-interrupt-condition
+     #+ecl ext:interactive-interrupt
+     #+allegro excl:interrupt-signal
+     () (progn
+          (format *error-output* "Aborting.~&")
+          (uiop:quit)))
     (error (c) (format t "Woops, an unknown error occured:~&~a~&" c))))
