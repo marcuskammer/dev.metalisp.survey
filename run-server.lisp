b3cc8d9a79724da1f699a10ec6b15290359778b3
Marcus Kammer <marcus.kammer@mailbox.org>
2024-07-02 17:39:39 +0200

    Check if slime exists

--- run-server.lisp
+++ run-server.lisp
@@ -1,3 +1,4 @@
-(load "~/slime/start-swank.lisp")
+(when (probe-file (merge-pathnames "slime/start-swank.lisp" (user-homedir-pathname)))
+  (load "~/slime/start-swank.lisp"))
 (ql:quickload :dev.metalisp.survey)
 (ml-survey:start)
