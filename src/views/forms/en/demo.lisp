bf706377173d1c88679d719188a162e31f628ae1
Marcus Kammer <marcus.kammer@mailbox.org>
2024-07-02 18:34:31 +0200

    Demo form satisfies to choicesp and questionp

--- src/views/forms/en/demo.lisp
+++ src/views/forms/en/demo.lisp
@@ -3,13 +3,17 @@
 (multi-form
   (:ask "What is your age range?"
    :group "demo-age-range"
+   :style "list-style-type:none"
    :choices (:single "18-24" "25-34" "35-44" "45-54" "55+"))
   (:ask "What is your gender?"
    :group "demo-gender"
+   :style "list-style-type:none"
    :choices (:single "Male" "Female" "Non-binary" "Prefer not to say" "Other" :text "Other"))
   (:ask "What is your profession?"
    :group "demo-profession"
+   :style "list-style-type:none"
    :choices (:text "Profession"))
   (:ask "What is your educational background?"
    :group "demo-edu"
+   :style "list-style-type:none"
    :choices (:text "Last Degree")))
