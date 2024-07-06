ee1d328f9799f28e527f4b42ac35edb315311d12
Marcus Kammer <marcus.kammer@mailbox.org>
2024-07-06 13:44:28 +0200

    Update css classes for survey view

--- dev.metalisp.survey.asd
+++ dev.metalisp.survey.asd
@@ -1,6 +1,6 @@
 (defsystem "dev.metalisp.survey"
   :description "A simple survey"
-  :version "0.1.6"
+  :version "0.1.7"
   :author "Marcus Kammer <marcus.kammer@metalisp.dev"
   :source-control "git@git.sr.ht:~marcuskammer/dev.metalisp.survey"
   :licence "MIT"
--- src/views/survey.lisp
+++ src/views/survey.lisp
@@ -13,15 +13,15 @@
       (:main :id "main-content"
 	         :class "container"
 	         (:p (format nil "ID: ~a" (ml-survey:survey-id survey)))
-	         (:h2 "Properties")
+	         (:h2 :class "py-3" "Properties")
 	         (ml-survey:survey-html survey)
 
 	         (when results-not-null
-               (:h2 "Questionnaire Results")
+               (:h2 :class "py-3" "Questionnaire Results")
 
                (if sus-results
                    (let ((count-answers (length (cdr (car sus-results)))))
-                     (:h3 "SUS")
+                     (:h3 :class "py-1" "SUS")
                      (:table :class "table table-hover"
                        (:caption "Questionnaire results table")
 		               (:thead
@@ -35,9 +35,12 @@
                               do (:tr (mapcar (lambda (data) (:td data)) row)))))))
 
                (loop for (type data) on results by #'cddr unless (eq type :sus)
-                     do (progn (:h3 (format nil "~a" type))
+                     do (progn (:h3 :class "py-1" (format nil "~a" type))
                                (loop for row in data
                                      do (:ul :class "list-group py-3"
                                              (loop for data in row
-                                                   do (:li :class "list-group-item"
+                                                   for i from 0
+                                                   do (:li :class (if (zerop i)
+                                                                      "list-group-item active"
+                                                                      "list-group-item")
                                                            data)))))))))))
