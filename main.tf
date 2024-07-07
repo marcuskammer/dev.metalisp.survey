c8b5558d4001b795789d5056901e4e2bb42e4e8f
Marcus Kammer <marcus.kammer@mailbox.org>
2024-07-07 19:39:42 +0200

    Fix naming resources in tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-resource "aws_vpc" "survey-buttler-vpc" {
-  cidr_block           = "10.123.0.0/16"
+resource "aws_vpc" "metalisp-survey-vpc" {
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    Name = "survey-buttler-vpc"
+    Name = "metalisp-survey-vpc"
   }
 }
 
-resource "aws_subnet" "survey-buttler-public-subnet" {
-  vpc_id                  = aws_vpc.survey-buttler-vpc.id
-  cidr_block              = "10.123.1.0/24"
+resource "aws_subnet" "metalisp-survey-public-subnet" {
+  vpc_id                  = aws_vpc.metalisp-survey-vpc.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-central-1"
   tags = {
-    Name = "survey-buttler-public"
+    Name = "metalisp-survey-public"
   }
 }
