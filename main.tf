7eb7389eb9a9b060aa7c4ed033810c4db24ed777
Marcus Kammer <marcus.kammer@mailbox.org>
2024-07-08 17:52:37 +0200

    Better naming for tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-resource "aws_vpc" "metalisp-survey-vpc" {
+resource "aws_vpc" "ml-survey-vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    Name = "metalisp-survey-vpc"
+    Name = "ml-survey-vpc"
   }
 }
 
-resource "aws_subnet" "metalisp-survey-public-subnet" {
-  vpc_id                  = aws_vpc.metalisp-survey-vpc.id
+resource "aws_subnet" "ml-survey-public-subnet" {
+  vpc_id                  = aws_vpc.ml-survey-vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-central-1"
   tags = {
-    Name = "metalisp-survey-public"
+    Name = "ml-survey-public"
   }
 }
